10dd2f76ca865e36fd6b2399dcf0b72c356240cf
srivatsavr02 <srivatsavr02@gmail.com>
2022-02-05 13:10:34 +0530

    done

--- index.js
+++ index.js
@@ -15,7 +15,13 @@ const app = express();
 dotenv.config();
 
 app.use(express.json());
-app.use(cors({ origin: true }));
+const cors = require('cors');
+const corsOptions ={
+    origin:'https://mern-myfirstblog.herokuapp.com/', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 mongoose
     .connect(process.env.MONGO_URL)
